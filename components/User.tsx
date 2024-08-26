b860458943210e8f6e754b6316c21e9b071e7b15
vindard <17693119+vindard@users.noreply.github.com>
2024-08-25 22:06:18 -0400

    refactor: redirect blink service imports

--- components/User.tsx
+++ components/User.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MeQuery } from '@/services/blink/generated';
+import { MeQuery } from '@/services/server/blink/generated';
 import { useEffect, useState } from 'react';
 
 const User = () => {
