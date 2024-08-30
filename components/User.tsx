c8fdf7ec330464f86b7d1213a5286aa85d637775
vindard <17693119+vindard@users.noreply.github.com>
2024-08-30 01:27:24 -0400

    chore: add price function to User component

--- components/User.tsx
+++ components/User.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import { btcSatPriceInTtd } from "@/services/client/blink/queries";
 import { MeQuery } from "@/services/common/blink/generated";
+import BigNumber from "bignumber.js";
 import { useEffect, useState } from 'react';
 
 const User = () => {
   const [userData, setUserData] = useState<MeQuery | null>(null);
+  const [usdBtcPrice, setUsdBtcPrice] = useState<string | null>(null);
+  const [ttdBtcPrice, setTtdBtcPrice] = useState<string | null>(null);
+  const [ttdUsdPrice, setTtdUsdPrice] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -31,12 +36,34 @@ const User = () => {
     fetchUserData();
   }, []);
 
+  const toLocaleString = (val: string) =>
+    parseFloat(val).toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  useEffect(() => {
+    const fetchRealtimePrice = async () => {
+      const prices = await btcSatPriceInTtd();
+      if (prices instanceof Error) {
+        setError(prices.message);
+        return;
+      }
+      setTtdBtcPrice(toLocaleString(prices.priceOfOneBtcInTtd));
+      setUsdBtcPrice(toLocaleString(prices.priceOfOneBtcInUsd));
+      setTtdUsdPrice(toLocaleString(prices.priceOfOneUsdInTtd));
+    };
+
+    fetchRealtimePrice();
+  }, []);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div>
       <h1>Welcome, {userData?.me?.username || userData?.me?.defaultAccount.id}!</h1>
+      <h2>Current price: TT${ttdBtcPrice} (US${usdBtcPrice} @ TT${ttdUsdPrice})</h2>
     </div>
   );
 };
